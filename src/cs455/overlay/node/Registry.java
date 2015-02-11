16d3f559e0984b996258af259916491db66a292d
tcqiuyu <tcqiuyu2@gmail.com>
2015-02-10 22:07:39 -0700

    Modify Protocol value & add command line input method for registry

--- src/cs455/overlay/node/Registry.java
+++ src/cs455/overlay/node/Registry.java
@@ -1,5 +1,7 @@
 package cs455.overlay.node;
 
+import java.io.IOException;
+
 import cs455.overlay.wireformats.Event;
 
 public class Registry implements Node{
@@ -10,4 +12,24 @@ public class Registry implements Node{
 		
 	}
 
+	public void listMessagingNode(){
+		// TODO
+	}
+	
+	public void setupOverlay(int overlayNum){
+		// TODO
+	}
+	
+	public void listRoutingTables(){
+		// TODO
+	}
+	
+	public void start(int messageNum){
+		// TODO
+	}
+	
+	public static void main(String args[]) throws IOException{
+		
+	}
+	
 }
